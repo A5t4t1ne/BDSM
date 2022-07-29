a37b3fae627639d0238d09562738e92b92700677
Jave01 <david.jaeggli@gmail.com>
2022-07-29 06:46:02 +0200

    asdf

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,33 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created database')
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "spotted-osmosis-overvalue"
-    
+    app.config['SQLALCHEMY_DATABSE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import Note, User
+
+    create_database(app)
 
     return app
