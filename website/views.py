b6dbc09e5e5be329abc82240c5a235e54ed18226
Jave <david.jaeggli@gmail.com>
2022-07-29 17:53:55 +0200

    basic login almost functioning

--- website/views.py
+++ website/views.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, login_user, logout_user, current_user
 
 views = Blueprint("views", __name__)
-
 
 @views.route('/')
 def redirect_to_home():
     return redirect(url_for('views.home'))
 
 @views.route("/home")
+@login_required
 def home():
     return render_template("home.html")
