f590869e422c003e31cb70047674cc1303e9daeb
Jave01 <david.jaeggli@gmail.com>
2022-08-24 17:43:21 +0200

    add basic file upload system

--- website/models.py
+++ website/models.py
@@ -9,13 +9,21 @@ class Note(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))   # one to many -> ForeignKey binds the attribute to an object
 
 
+class Hero(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hero_name = db.Column(db.String(150))
+    hero_path = db.Column(db.String(1000))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(150))
+    access_lvl = db.Column(db.String(50))
+    email = db.Column(db.String(150))   # not used yet
+    heroes_path = db.Column(db.String(1000))
 
     # one to many -> relationship connects the "one" to another class of which it can possess multiples. 
     # notes will be a list type variable with all Note-objects created by the indivial user 
     notes = db.relationship('Note')
-    
-    # email = db.Column(db.String(150))     -> for later use
+    heroes = db.relationship('Hero')
