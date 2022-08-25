837d5f6da4360e499ba724dcb8e242abf024306e
Jave <david.jaeggli@gmail.com>
2022-08-25 11:47:31 +0200

    adjust 'create_app()' variables

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_manager
+from flask_login import LoginManager
 import os
 
 
@@ -13,33 +13,35 @@ def create_database(app, db_dir):
         print('Created database')
 
 
-def create_app(PATH_TO_DB_FOLDER="", DB_NAME="database.db"):
+def create_app(db_path="", db_name="database.db", heroes_path="heroes"):
     app.config['SECRET_KEY'] = "spotted-osmosis-overvalue"
 
     basedir = os.path.abspath(os.path.dirname(__file__))
-    db_dir = os.path.join(basedir, DB_NAME)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_dir
+    db_dir = os.path.join(basedir, db_path, db_name)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_dir       # dir to database
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     app.config['MAX_CONTENT_PATH'] = 1_000_000
-    app.config['UPLOAD_FOLDER'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'heroes') # upload folder is [main.py-location]/heroes/
-    app.config['ALLOWED_EXTENSIONS'] = {'json', 'png'}
+    app.config['UPLOAD_FOLDER'] = os.path.join(basedir, heroes_path)    # upload folder is [main.py-location]/heroes/
+    app.config['ALLOWED_EXTENSIONS'] = {'json'}
+
     db.init_app(app)
 
-
+    # include other flask routes and connect them
     from .views import views
     from .auth import auth
-    from .upload import upload
+    # from .upload import upload
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    app.register_blueprint(upload, url_prefix='/')
+    # app.register_blueprint(upload, url_prefix='/')
 
     from .models import Note, User
 
     create_database(app, db_dir)
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = 'auth.login'     # default page to call if user isn't logged in
     login_manager.init_app(app=app)
     login_manager.login_message = ""
 
