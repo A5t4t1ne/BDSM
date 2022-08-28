6899a99cbe6bb967bdc4a787c5b55103c61f5c2f
Jave01 <david.jaeggli@gmail.com>
2022-08-28 21:17:12 +0200

    add decode all

--- website/tools/upload.py
+++ website/tools/upload.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 from flask_login import current_user
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField, MultipleFileField
@@ -12,9 +13,34 @@ class UploadFileForm(FlaskForm):
     submit = SubmitField("Commit")
 
 
-def is_valid_file_type(filename:str):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+def is_valid_hero(file):
+    filename = file.filename
+
+    hero = json.load(file)
+
+    if '.' not in filename or \
+        filename.rsplit('.', 1)[1].lower() not in app.config['ALLOWED_EXTENSIONS']:
+        return False
+
+    print("2. Step")
+
+    # convert version X.Y.Z to XY
+    version = hero['clientVersion'].split('.')[:2]
+    version = int(version[0])*10 + int(version[1])
+
+    name = hero.get('name', None)
+    attr = hero.get('attr', None)
+    race = hero.get('r', None)
+    acti = hero.get('activatable', None)
+    belo = hero.get('belongings', None)
+    
+    return version > 10 and \
+        name != "" and \
+        name != None and \
+        attr != None and \
+        race != None and \
+        acti != None and \
+        belo != None
 
 
 def secure_save(file):
@@ -27,20 +53,18 @@ def secure_save(file):
     
     file:     raw file
     """
-    if not is_valid_file_type(file.filename):
+    if not is_valid_hero(file):
         return False
 
-    file_path = os.path.join(current_user.heroes_path, secure_filename(file.filename))
+    # TODO: change to directly create new file and never save the given one
 
-    file.save(file_path)
+    # file_path = os.path.join(current_user.heroes_path, secure_filename(file.filename))
 
-    with open(file_path, 'r') as f:
-        hero = json.load(f)
+    # file.save(file_path)
 
-    # print(f"\n\nHero data: {hero}\n\n")
+    # with open(file_path, 'r') as f:
+    #     hero = json.load(f)
 
-    if not 'name' in hero:
-        return False
 
     # hero_name = hero['name']
     # new_hero = Hero(hero_name=hero_name, hero_path=file_path, user_id=current_user.id)
