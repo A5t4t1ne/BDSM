c3320af0f2c7908f60e8c491f3630a2d95df2b82
Jave01 <david.jaeggli@gmail.com>
2022-09-09 20:54:34 +0200

    move configs to config.json

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,11 @@
+from msilib.schema import Error
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import json
 import os
 
+CONFIG_PATH = 'website/config.json'
 
 db = SQLAlchemy()
 app = Flask(__name__)
@@ -14,13 +17,21 @@ def create_database(app, db_dir):
         print('Created database')
 
 
-def create_app(db_path="", db_name="database.db", upload_folder="heroes"):
-    key = os.environ.get('FLASK_SECRET_KEY')
-    app.config['SECRET_KEY'] = key
+def create_app(db_name="database.db", upload_folder="heroes"):
+    if not os.path.isfile(CONFIG_PATH):
+        raise Error("Create a config.json file in website directory with a 'SECRET_KEY' and an 'ACCESS_CODE' property")
+
+    with open(CONFIG_PATH, 'r') as f:
+        config = json.load(f)
+        if not 'SECRET_KEY' in config or 'ACCESS_CODE' not in config:
+            raise Error("You need to define the two configurations 'ACCESS_CODE' and 'SECRET_KEY in config.json'")
+    
+    app.config['SECRET_KEY'] = config['SECRET_KEY']
+    app.config['ACCESS_CODE'] = config['ACCESS_CODE']
 
     # get abs path starting from this file location
     basedir = os.path.abspath(os.path.dirname(__file__))
-    db_dir = os.path.join(basedir, db_path, db_name)
+    db_dir = os.path.join(basedir, db_name)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_dir
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
