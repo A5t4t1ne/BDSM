55f3138722095f285ff2d371905476bb5f821fb3
Jave01 <david.jaeggli@gmail.com>
2023-01-14 23:57:37 +0100

    mid save

--- src/app.py
+++ src/app.py
@@ -4,5 +4,5 @@ application = create_app()
 
 
 if __name__ == "__main__":
-    application.run(debug=True)
-    # application.run(debug=False, host="0.0.0.0", port=5000)
+    application.run(debug=True, port=80)
+    # application.run(debug=False, host="0.0.0.0", port=80)
